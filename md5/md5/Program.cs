6b7a1af7529fb396d0e721ed8790083cdeff758b
Alina Kohnovich <Alina Kohnovich@DESKTOP-8JE05TL>
2017-06-03 00:24:02 +0300

    изменен main

--- md5/md5/Program.cs
+++ md5/md5/Program.cs
@@ -6,14 +6,7 @@ public class Program
     public static void Main()
     {
 
-        MD5.MD5 md = new MD5.MD5();
-
-        for(;;)
-        { 
-            md.Value = Console.ReadLine();
-            Console.WriteLine( "MD5 Value =" + md.Value );
-            Console.WriteLine();
-        }
+       
     }
 }
 
